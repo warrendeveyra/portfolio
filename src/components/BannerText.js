dfb777f6c08129619efc140aa6f8fc1effba4d0c
warrendeveyra <82423898+warrendeveyra@users.noreply.github.com>
2021-05-23 17:28:24 +0800

    quick fix

--- src/components/BannerText.js
+++ src/components/BannerText.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography, Box, Link } from "@material-ui/core";
+import { Grid, Typography, Box } from "@material-ui/core";
 import { useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import clsx from "clsx";
